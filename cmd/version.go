44a36e7dcfdd8715f9c52efb503d5b9ae9c39e9b
Geert Eltink <geert.eltink@gmail.com>
2019-11-26 20:46:36 +0100

    feat: add version

--- cmd/version.go
+++ cmd/version.go
@@ -7,15 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of GitOps",
-	Long:  "All software has versions. This is GitOps's.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("GitOps %s compiled with %v on %v/%v\n", "0.2.0", runtime.Version(), runtime.GOOS, runtime.GOARCH)
-	},
-}
+var (
+	// Version contains the build version
+	Version string = "0.2.1"
+
+	// versionCmd represents the version command
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of GitOps",
+		Long:  "All software has versions. This is GitOps's.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("GitOps %s compiled with %v on %v/%v\n", Version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
